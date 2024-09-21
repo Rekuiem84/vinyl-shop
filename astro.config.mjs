eaab4cc15c45348b21b534fb5c9c84cf67ca00fb
Rekuiem84 <morinijoshua@gmail.com>
2024-09-21 23:45:55 +0200

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-	site: "https://rekuiem84.github.io",
-	base: "/vinyl-shop",
-});
+export default defineConfig({});
